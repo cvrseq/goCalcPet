4bc132ffc4a4ac8981337a4b7b7633a359077417
anger issues <skuenemy@gmail.com>
2025-12-05 01:52:00 +0300

    next

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+type requestBody struct {
+	Task string `json:"task"`
+}
+
+var task string
+
+func createHandler(w http.ResponseWriter, r *http.Request) error {
+	// pass
+}
 
 func main() {
-	fmt.Println("soon")
+
 }
