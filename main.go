ad6333ba786b4fe43f19f5f51f62fc0f522b3c02
anger issues <skuenemy@gmail.com>
2025-12-06 02:59:54 +0300

    update

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,10 +13,35 @@ type requestBody struct {
 
 var task string
 
-func createHandler(w http.ResponseWriter, r *http.Request) error {
-	// pass
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	var rq requestBody
+
+	if err := json.NewDecoder(r.Body).Decode(&rq); err != nil {
+		fmt.Printf("%v", "Error not decode from Body to reqBody struct")
+	}
+
+	//if r.Method != r.PostFormValue(task) {
+	//	fmt.Printf("%v, \n", http.StatusBadRequest)
+	//}
+
+	fmt.Println("successful decode")
+
+	task = rq.Task
+
+	fmt.Println("json data write in the task variable")
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
 }
 
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/endpoint", createHandler)
+
+	fmt.Println("Server is working...")
+	fmt.Println("http://localhost:8080/endpoint")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
