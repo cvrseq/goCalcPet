53bb5fceb60dc316d3bae2e202a295c6d0476c5e
anger issues <skuenemy@gmail.com>
2025-12-27 21:32:08 +0300

    update, added db postgres

--- go.mod
+++ go.mod
@@ -4,6 +4,12 @@ go 1.24.2
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.13.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -13,6 +19,9 @@ require (
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,85 +3,136 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 )
 
-type requestBody struct {
-	Task string `json:"task"`
-	ID   string `json:"id"`
+var db *gorm.DB
+
+func initDB() {
+	dataSrcName := "host=localhost user=postgres password=yourpassword dbname=postgres port=5000 sslmode=disable"
+
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dataSrcName), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&requestBody{}); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+	}
 }
 
-var task string
+type requestBody struct {
+	Task   string `gorm:"column:task" json:"task"`
+	ID     uint   `gorm:"primaryKey" json:"id"`
+	IsDone bool   `gorm:"column:is_done" json:"is_done"`
+}
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
-	var rq requestBody
-	if err := json.NewDecoder(r.Body).Decode(&rq); err != nil {
-		log.Fatal(err)
+
+	var task requestBody
+
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, "Could not decode in the create", http.StatusBadRequest)
 		return
 	}
 
 	fmt.Println("successful decode")
 
-	task = rq.Task
+	task.ID = 0
+
+	fmt.Println("Trying create variable and write db")
+	if err := db.Create(&task).Error; err != nil {
+		http.Error(w, "Could not create in db", http.StatusInternalServerError)
+		return
+	}
 
 	fmt.Println("json data write in the task variable")
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(task); err != nil {
+		http.Error(w, "Could not decode in the create", http.StatusNotFound)
+		return
+	}
 
 }
 
 func getHandlers(w http.ResponseWriter, r *http.Request) {
+	var task []requestBody
+
+	if err := db.Find(&task).Error; err != nil {
+		http.Error(w, "Could not find on db", http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(task); err != nil {
-		log.Fatal(err)
+		http.Error(w, "Could not find on db", http.StatusNotFound)
 		return
 	}
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
+	var task requestBody
+	id := r.PathValue("id")
 
-	if n, err := fmt.Fprintf(w, "%s", id); err != nil {
-		fmt.Println("not change var", n)
+	var rq struct {
+		Task   *string `json:"task"`
+		IsDone *bool   `json:"is_done"`
 	}
 
-	fmt.Println(id)
-
-	var rq requestBody
-
 	if err := json.NewDecoder(r.Body).Decode(&rq); err != nil {
-		log.Fatal(err)
+		http.Error(w, "Could not decode in the update", http.StatusBadRequest)
 		return
 	}
 
 	fmt.Println("successful decode")
 
-	task = rq.Task
+	if err := db.First(&task, "id = ?", id).Error; err != nil {
+		http.Error(w, "Could not first on db", http.StatusInternalServerError)
+		return
+	}
+
+	if rq.Task != nil {
+		task.Task = *rq.Task
+	}
+
+	if rq.IsDone != nil {
+		task.IsDone = *rq.IsDone
+	}
+
+	if err := db.Save(&task).Error; err != nil {
+		http.Error(w, "Could not save on db", http.StatusInternalServerError)
+		return
+	}
 
 	fmt.Println("json data update in the task variable")
 
 	w.Header().Set("Content-Type", "application/json")
 
 	if err := json.NewEncoder(w).Encode(task); err != nil {
-		log.Fatal(err)
+		http.Error(w, "Could not encode in the update", http.StatusNotFound)
 		return
 	}
 
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	var rq requestBody
+	id := r.PathValue("id")
 
 	fmt.Println("successful decode in the delete")
 
-	if id == rq.ID {
-		task = ""
+	if err := db.Delete(&requestBody{}, id).Error; err != nil {
+		http.Error(w, "Could not delete on db", http.StatusInternalServerError)
+		return
+
 	}
 
 	w.WriteHeader(http.StatusNoContent)
@@ -89,6 +140,7 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	initDB()
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /tasks", createHandler)
